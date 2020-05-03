fa6a8f32e07a3edc424608ea9534a83ed90aa24a
m3yevn <mmm.myintmyat@gmail.com>
2020-05-03 22:21:09 +0800

    ✂️ remove: redux dev tools extension because of browser

--- src/utils/store.js
+++ src/utils/store.js
@@ -8,8 +8,6 @@ const middleware = [thunkMiddleware];
 export const store = createStore(
   rootReducer,
   initialState, compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleware)
   )
 );
