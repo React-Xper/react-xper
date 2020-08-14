30fab5a1a10d8f7377efc707ff6f95ea2d89e67d
m3yevn <mmm.myintmyat@gmail.com>
2020-08-14 12:52:17 +0800

    add: ie support for reacthesis

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './App';
-import * as serviceWorker from './serviceWorker';
-import { Provider as StoreProvider } from 'react-redux';
-import { store } from './utils/store';
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Provider as StoreProvider } from "react-redux";
+import { store } from "./utils/store";
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,10 +13,10 @@ ReactDOM.render(
       <App />
     </StoreProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.unregister();
