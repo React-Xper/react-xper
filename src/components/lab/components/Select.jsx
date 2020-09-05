68de210f7e50a977bb73790787f1c2c9a06bff8a
m3yevn <mmm.myintmyat@gmail.com>
2020-09-05 14:09:31 +0800

    wrap: emoji inside string

--- src/components/lab/components/Select.jsx
+++ src/components/lab/components/Select.jsx
@@ -6,10 +6,10 @@ export function Select() {
     <div>
       <div>What do you think about this?</div>
       <SelectComponent title="Select Idea 🤔" style={{ width: "300px" }}>
-        <option value="">None ❌</option>
-        <option value="1">Reacthesis-UI is good🤗</option>
-        <option value="2">Reacthesis-UI is great😎</option>
-        <option value="3">Reacthesis-UI is love😘</option>
+        <option value="">{"None ❌"}</option>
+        <option value="1">{"Reacthesis-UI is good🤗"}</option>
+        <option value="2">{"Reacthesis-UI is great😎"}</option>
+        <option value="3">{"Reacthesis-UI is love😘"}</option>
       </SelectComponent>
     </div>
   );
