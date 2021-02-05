30b23949b9a283e7fd243edabf8279ff2007e2da
m3yevn <mmm.myintmyat@gmail.com>
2021-02-05 15:13:32 +0800

    change: banner

--- src/components/shared/Banner.jsx
+++ src/components/shared/Banner.jsx
@@ -7,9 +7,9 @@ export function Banner() {
   return (
     <Box width="100vw" justify="center" className="reacthesis-banner">
       <div>
-        Black Lives Matter. {size === "small" && <br />}
-        <a href="https://support.eji.org/give/153413/#!/donation/checkout">
-          Support Equal Justice Initiative
+        Myanmar Votes Matter. {size === "small" && <br />}
+        <a href="https://www.channelnewsasia.com/news/asia/myanmar-doctors-medics-civil-disobedience-calls-activists-14103732">
+          Support Myanmar Civil Disobedience
         </a>
       </div>
     </Box>
